2c7a3b70925ef26ca22eaef2a9895423518d2ff6
Albin Calais <albincalais@gmail.com>
2023-07-15 20:13:48 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    install_requires=['Pillow'],
+    author='Akahara',
+    name='commandlinetools',
+    version='1.0.0',
+    description='Random command line tools',
+    scripts=['catimg'],
+)
